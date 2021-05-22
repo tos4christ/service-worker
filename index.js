ace428b3454e6defb922d49e919541ba9aafbcd3
tos4christ <gnztrades@gmail.com>
2021-05-22 18:52:54 +0100

    register sw

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 
 if ('serviceWorker' in navigator) {
-    
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then( registration => {
+            // Registration successful
+            console.log('Service Worker Registration successful with scope', registration.scope);
+        }, err => {
+            // registeration failed
+            console.log('Service Worker Registration failed', err);
+        });
+    });
 }
