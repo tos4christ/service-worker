2232296d527da67d4137a508dd9b38741c4ccb00
tos4christ <gnztrades@gmail.com>
2021-05-22 19:37:44 +0100

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,11 @@
 self.addEventListener('install', function(event) {
     // Perform install steps
-    
+    // 1. Open a cache.  2. Cache our files
+    // confirm if all the required assets are cached or not
+    const CACHE_NAME = 'tos4christ-cache-v1';
+    const urlsToCache = [
+        '/',
+        '/index.css',
+        '/index.js'
+    ]
 });
