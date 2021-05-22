702572603de7a2b7c28e90ac9a37b42c60ec2641
tos4christ <gnztrades@gmail.com>
2021-05-22 21:29:15 +0100

    add more urls for sw to register

--- sw.js
+++ sw.js
@@ -6,6 +6,20 @@ const urlsToCache = [
     '/index.css',
     '/index.js'
 ];
+const CACHE_ARRAY = [
+    {
+        name: 'hello-world',
+        urls: [
+            '/hello-world.html'
+        ]
+    },
+    {
+        name: 'about-us',
+        urls: [
+            '/about-us.html'
+        ]
+    }
+]
 
 // Install Service Workers
 self.addEventListener('install', function(event) {
@@ -28,7 +42,26 @@ self.addEventListener('fetch', function(event) {
                 if (response) {
                     return response;
                 }
-                return fetch(event.request);
+                return fetch(event.request).then(
+                    function(response) {
+                        // Check if we received a valid response
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        // IMPORTANT: Clone the response. A response is a stream
+                        // and because we want the browser to consume the response
+                        // as well as the cache consuming the response, we need
+                        // to clone it so we have two streams.
+                        var responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+                        
+                        return response;
+                    }
+                );
             })
     );
 });
