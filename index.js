7e93be66bd47523a455403849d2693c0dba6bbca
tos4christ <gnztrades@gmail.com>
2021-05-24 08:15:31 +0100

    add extend folder and move sw.js in

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js').then( function(registration) {
+        navigator.serviceWorker.register('/extend/sw.js').then( function(registration) {
             // Registration successful
             console.log('Service Worker Registration successful with scope', registration.scope);
         }, function(err) {
